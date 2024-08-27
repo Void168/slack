95696333c86b06240a586f695229a722df0d6d58
LPHung <phuhung16820@gmail.com>
2024-08-28 00:44:07 +0700

    create channel

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 
 import { CreateWorkSpaceModal } from "@/features/workspace/components/create-workspace-modal";
+import { CreateChannelModal } from "@/features/channels/components/create-channel-modal";
 
 export const Modals = () => {
   const [mounted, setMounted] = useState(false);
@@ -15,6 +16,7 @@ export const Modals = () => {
   
   return (
     <>
+      <CreateChannelModal />
       <CreateWorkSpaceModal />
     </>
   );
