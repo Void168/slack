0fb4788babd42d3ba88184a2b717e747bb7d3951
LPHung <phuhung16820@gmail.com>
2024-08-27 18:27:14 +0700

    create workspace sidebar header

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -2,6 +2,7 @@ import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkSpace } from "@/features/workspace/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { AlertTriangle, Loader } from "lucide-react";
+import { WorkspaceHeader } from "./workspace-header";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
@@ -25,12 +26,14 @@ export const WorkspaceSidebar = () => {
     return (
       <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
         <AlertTriangle className="size-5 text-white" />
-        <p className="text-white text-sm">
-            Workspace not found
-        </p>
+        <p className="text-white text-sm">Workspace not found</p>
       </div>
     );
   }
 
-  return <div>WorkspaceSidebar</div>;
+  return (
+    <div className="flex flex-col bg-[#5E2C5F] h-full">
+      <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"}/>
+    </div>
+  );
 };
